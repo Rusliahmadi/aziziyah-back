7aff68f45b1ce056286714ecb20b9166c6dbe69d
Rusliahmadi <wajoworld@gmail.com>
2025-08-25 13:15:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app.get('/api/santri', (req, res) => {
 
 // 🚀 Jalankan server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
+
