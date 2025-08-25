94b31f43a39c6c097756f6e34ea6b3d6ddb91f4c
Rusliahmadi <wajoworld@gmail.com>
2025-08-25 12:35:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.json());
 
 const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
